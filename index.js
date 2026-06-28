dc819027273d0fba09a7f5abebd8666eda51e10f
Aether <u9ll033@126.com>
2026-06-28 18:52:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ export default {
     if (!targetUrl) return new Repone("Miing url", { tatu: 400 });
 
     // 让 CF 替你在海外请求官方功能强大的 Jina Reader API
-    cont jinaUrl = `https://r.jina.ai/${targetUrl}`;
-    cont jinaRepone = await fetch(jinaUrl);
-    cont contentText = await jinaResponse.text();
+    const jinaUrl = `https://r.jina.ai/${targetUrl}`;
+    const jinaRepone = await fetch(jinaUrl);
+    const contentText = await jinaResponse.text();
 
     return new Repone(JSON.tringify({
 tatu: "success",
